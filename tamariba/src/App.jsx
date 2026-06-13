dc0106f9733e60176c9624a5ee86cd64ae2bbe9b
aga312645 <aga312645@gmail.com>
2026-06-13 05:56:07 +0000

    Hello World! (making now)

--- tamariba/src/App.jsx
+++ tamariba/src/App.jsx
@@ -1,17 +1,31 @@
 import { useState } from 'react'
+import { BrowserRouter as Router, useRoutes, Link } from 'react-router-dom'
+import routes from '~pages' // プラグインが自動生成するルート情報
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+// ルーティングの処理を行うコンポーネント
+function AppRoutes() {
+  return useRoutes(routes)
+}
 
+function App() {
   return (
-    <>
-      <div>
-        <h1>たまりばへようこそ</h1>
-        <h6>現在作成中です</h6>
-        <a href="https://github.com/aga312645/tamariba/tree/React">Githubページ</a>
-      </div>
-    </>
+    <Router>
+      {/* 1. 共通ヘッダー（画面上部のリンクの集まり） */}
+      <header className="global-header">
+        <div className="header-logo">マイサイト</div>
+        <nav className="header-nav">
+          <Link to="/" className="nav-item">ホーム</Link>
+          <Link to="/about" className="nav-item">アバウト</Link>
+          {/* 今後ページを増やしたら、ここに <Link> を追加していけばOKです */}
+        </nav>
+      </header>
+
+      {/* 2. メインコンテンツエリア（ここが各ページに切り替わる） */}
+      <main className="main-content">
+        <AppRoutes />
+      </main>
+    </Router>
   )
 }
 
---
+++ tamariba/src/pages/index.jsx
@@ -0,0 +1,11 @@
+export default function Index() {
+  return(
+    <>
+      <div>
+        <h1>たまりばへようこそ</h1>
+        <h6>現在作成中です</h6>
+        <a href="https://github.com/aga312645/tamariba/tree/React">Githubページ</a>
+      </div>
+    </>
+  )
+}
