579af131d86e5f659bc4064cbb6d3cc347c913a0
aga312645 <aga312645@gmail.com>
2026-06-13 06:01:32 +0000

    Hello World! (making now)

--- tamariba/src/App.jsx
+++ tamariba/src/App.jsx
@@ -1,7 +1,17 @@
 import { useState } from 'react'
+import React from 'react'
 import { BrowserRouter as Router, useRoutes, Link } from 'react-router-dom'
-import routes from '~pages' // プラグインが自動生成するルート情報
 import './App.css'
+
+// 💡 プラグインを使わず、Vite標準機能で pages フォルダ内のファイルを自動で読み込む
+const pages = import.meta.glob('./pages/*.jsx', { eager: true })
+const routes = Object.keys(pages).map((path) => {
+  const name = path.match(/\.\/pages\/(.*)\.jsx$/)[1]
+  return {
+    path: name === 'index' ? '/' : `/${name.toLowerCase()}`,
+    element: React.createElement(pages[path].default),
+  }
+})
 
 // ルーティングの処理を行うコンポーネント
 function AppRoutes() {
