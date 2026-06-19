f79b3d544ee33e091f35d7403e5c97b10f809b6a
aga312645 <aga312645@gmail.com>
2026-06-19 11:26:51 +0000

    Hello World! (making now)

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Tldraw } from '@tldraw/tldraw'
+import '@tldraw/tldraw/tldraw.css'
+import './App.css'
+
+function App() {
+  return (
+    <div className="tldraw-container">
+      <Tldraw />
+    </div>
+  )
+}
+
+export default App
