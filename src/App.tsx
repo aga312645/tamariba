49bc853bfc5ba33da14ebc91af3fbccc64c676c7
aga312645 <aga312645@gmail.com>
2026-06-19 12:44:52 +0000

    Hello World! (making now)

--- src/App.tsx
+++ src/App.tsx
@@ -45,12 +45,6 @@ function App() {
   if (loading) {
     return <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>読み込み中...</div>
   }
-
-  return (
-    <div className="tldraw-container">
-      <Tldraw store={store} licenseKey="non-commercial" />
-    </div>
-  )
 }
 
 export default App
